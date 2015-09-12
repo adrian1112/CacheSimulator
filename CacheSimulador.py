22b9e5c332b4fbe6765dba11832da4fff12a1c7a
Adrian Aguilar <adrian11_12@hotmail.com>
2015-09-11 23:31:13 -0500

    algoritmo OPTIMO terminado

--- CacheSimulador.py
+++ CacheSimulador.py
@@ -1,51 +1,58 @@
-import sys
+# --coding: utf-8 --
+#!/usr/bin/env python
 
-# Para obtener los parametros  enviados mediante consola
-count=0
-doc=''
-tam=0
-tipo=''
-def RecorrerDoc(documento):
-	print tam
-	d=(linea for i,linea in enumerate(documento) if i<=tam)
-	for l in d:
-		print l
-		continue
-	return
+from sys import argv 
+
+input_file = argv[1]
+algoritmo= argv[2]
+tam_cache=argv[3]
+f = open(input_file)
+pages = f.read()
+f.close()
 
 
-def ObtenerParametros(sys):
-	for x in sys:
-		global count,doc,tam,tipo
-		if count == 0:
-			print x
-			count=count+1
+def Comparar(pag_en_Cache,pages):
+	try:
+		index=pages.index(pag_en_Cache)
+		return index
+	except:
+		return len(pages)
+
+def Optimal(pages,N):
+	diccionario = {} 
+	pages = pages.split("\n") 
+	#pages = pages.split() 
+	tamCache = N   
+	cache = [] 		 
+	p_f = 0 
+	print("Evaluando una caché "+algoritmo+" con "+ tam_cache+" entradas. "+"\n")
+	for i in range(len(pages)):
+		page = pages[i]	
+		if len(cache) < tamCache: 
+			if not(page in cache):
+				cache.append(page)
+				p_f += 1 # Page Fault.
 		else:
-			if count == 1:
-				doc=x
-				print x
-				count=count+1
-			else:
-				if count == 2:
-					tipo=x
-					print x
-					count=count+1
-				else:
-					if count == 3:
-						tam=int(x)
-						print x
-						count=count+1
-	print "Evaluando una cache "+ tipo +" con "+ str(tam) +" entradas."
-	print "Resultados:"
-	return
+			if not(page in cache):
+				indice = 0 
+				for j in range(tamCache):
+					mayor = Comparar(cache[j],pages[i:]) 
+					if (mayor > indice):
+						indice = mayor
+					else:
+						continue
+					diccionario[indice] = cache[j] 
+				paginaRemover = diccionario.get(indice) 
+				try:
+					cache[cache.index(paginaRemover)]=page
+					p_f += 1 
+				except:
+					continue
+	return p_f
+			
 
-ObtenerParametros(sys.argv)
 
-archivo=open(doc, 'r')
 
-print "funca"
-RecorrerDoc(archivo)
 
-archivo.close()
-
-	
+p_fs = Optimal(pages,int(tam_cache))
+print'Algortmo de reemplazo Optmo, Page Faults: ' + str(p_fs) 
