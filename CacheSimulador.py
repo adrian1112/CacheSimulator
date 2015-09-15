14cd9c0fa347905b11684b5ca528b4cb71bb0264
Marvincin <arthas_2301_lich@live.com>
2015-09-15 11:13:12 -0500

    UnirLRU
    Añadir codigo de LRU

--- CacheSimulador.py
+++ CacheSimulador.py
@@ -4,6 +4,38 @@
 import time
 import sys
 
+class LRUCache:
+    def __init__(self, capacidad,fallos):
+        self.cache = collections.OrderedDict()
+        self.capacidad = capacidad
+        self.fallos = fallos
+
+    def obtener(self, clave):
+        try:
+            valor = self.cache.pop(clave)
+            self.cache[clave] = valor
+            return valor
+        except KeyError:
+            return -1
+
+    def establecer(self, clave, valor):
+		
+        try:
+            self.cache.pop(clave)            	
+        except KeyError:
+        		self.fallos +=1
+        		if len(self.cache) >= self.capacidad:
+        			self.cache.popitem(last=False)
+        self.cache[clave] = valor
+        
+class Tiempo:
+    def __enter__(self):
+        self.start = time.clock()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.clock()
+        self.interval = self.end - self.start
 
 
 
@@ -87,4 +119,31 @@ if __name__ == "__main__":
 		porce = 37
 		print 'Resultados:' 
 		print 'Miss rate:\t%0.2f%c (%d misses out of %d references)' % (valor,porce,p_fs,cantidad)
-		print 'Miss rate (warm cache):\t%0.2f%c (%d misses out of %d references)' % (valorHit,porce,p_fs-int(tam_cache),cantidad)  
+		print 'Miss rate (warm cache):\t%0.2f%c (%d misses out of %d references)' % (valorHit,porce,p_fs-int(tam_cache),cantidad)
+	
+	if algoritmo == "LRU":
+		entrada = tam_cache
+		fallos = 0
+		cantidad = 0
+		contenido = list()
+		archivo = open(input_fil,"r")
+		print 'Evaluando una cache LRU con %d entradas.' % int(entrada) 
+		lru = LRUCache(int(entrada),fallos,warmCache)
+		with Tiempo() as t:
+			linea=archivo.readline()
+			sentencia=linea.rstrip("\n")
+			lru.establecer(sentencia,0)
+			cantidad +=1
+    		while linea != "":
+        		linea=archivo.readline()
+        		sentencia=linea.rstrip("\n")
+        		lru.establecer(sentencia,0)
+        		cantidad +=1	
+		print('LRU tomo %.03f sec' % t.interval)
+		base = lru.fallos
+		valor = float((float(base)/float(cantidad))*100)
+		valorHit = float(float(base-(len(lru.cache)))/float(cantidad)*100)
+		porce = 37
+		print 'Resultados:' 
+		print 'Miss rate:\t%0.2f%c (%d misses out of %d references)' % (valor,porce,lru.fallos,cantidad)
+		print 'Miss rate (warm cache):\t%0.2f%c (%d misses out of %d references)' % (valorHit,porce,cantidad-lru.fallos-len(lru.cache),cantidad) 
